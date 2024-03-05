a418c717daeb2c945050b1b9c01f04f3a526700e
Tomas Kennedy <tomasmkennedy@gmail.com>
2024-03-05 15:15:16 -0500

    Added Unit Tests

---
+++ src/week1/TodosSolution/TodosApi.UnitTests/FormattingNames.cs
@@ -0,0 +1,15 @@
+﻿namespace TodosApi.UnitTests;
+public class FormattingNames
+{
+    [Theory]
+    [InlineData("Bob", "Smith", "Smith, Bob")]
+    [InlineData("Jill", "Jones", "Jones, Jill")]
+    public void NamesAreFormattedProperly(string firstName, string lastName, string expected)
+    {
+        var utils = new Utils();
+
+        var fullName = utils.FormatName(firstName, lastName);
+
+        Assert.Equal(expected, fullName);
+    }
+}
