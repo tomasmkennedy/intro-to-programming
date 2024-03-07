37205fd2dfc7f22333c4f5e8518126c41f9df645
Tomas Kennedy <tomasmkennedy@gmail.com>
2024-03-07 10:07:52 -0500

    Added gold customer test

---
+++ src/week1/BankingSolution/Banking.UnitTests/Accounts/GoldCustomerDeposits.cs
@@ -0,0 +1,24 @@
+﻿using Bank;
+
+namespace Banking.UnitTests.Accounts;
+public class GoldCustomerDeposits
+{
+    [Theory]
+    [InlineData(100, 110)]
+    [InlineData(42.23, 46.453)]
+    public void MakingADespositIncreasesBalance(decimal amountToDeposit, decimal expected)
+    {
+        // Given
+        var account = new BankAccount();
+        // Get Balance is a "Query" - we are asking it for something.
+        var openingBalance = account.GetBalance();
+
+        // When
+        // WTCYWYH 
+        // Command - Telling it to do some work.
+        account.Deposit(amountToDeposit);
+
+        // Then
+        Assert.Equal(openingBalance + expected, account.GetBalance());
+    }
+}
